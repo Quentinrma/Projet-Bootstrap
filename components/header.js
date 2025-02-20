23b867eeb14887925dac805d0e9b7e662764d076
Quentin.rma <quentin.romera@outlook.fr>
2025-02-20 10:49:20 +0100

    Add header and footer components; include default image and update index.html structure

---
+++ components/header.js
@@ -0,0 +1,64 @@
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">
+      <img
+        src="./assets/img/logo QR.png"
+        alt="Quentin.rma"
+        class="img-responsive"
+        style="width: 50px; height: 50px"
+      />
+    </a>
+
+    <div class="btn-group"></div>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="./index.html">
+            Accueil
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./About.html">
+            About
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            CV
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="row mb-5">
+    <div class="m-auto" role="group">
+      <a href="#" class="btn">
+        <i
+          class="bi bi-instagram bi-4x text-dark"
+          style="font-size: 1.5rem"
+        ></i>
+      </a>
+      <a href="#" class="btn">
+        <i class="bi bi-linkedin bi-4x text-dark" style="font-size: 1.5rem"></i>
+      </a>
+      <a href="#" class="btn">
+        <i class="bi bi-facebook bi-4x text-dark" style="font-size: 1.5rem"></i>
+      </a>
+      <a href="#" class="btn">
+        <i class="bi bi-github bi-4x text-dark" style="font-size: 1.5rem"></i>
+      </a>
+    </div>
+  </div>
+</header>
